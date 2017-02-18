34e79208dba5dd1d6824c5eef167dd0a44b318db
Lucifer2016 <1980408063@qq.com>
2017-02-18 12:48:38 +0800

    second commit

---
+++ src/hiworld/Activator.java
@@ -0,0 +1,32 @@
+package hiworld;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+		System.out.println("my hiworld bundle is start!");
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+		System.out.println("my helloworld bundle is stop!");
+	}
+
+}
